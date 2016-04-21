eabde481b49d1ba0560ce732aa21d5265c0841fb
iua713 <ioana.mohanu@gmail.com>
2016-04-21 16:27:19 +0200

    Update isObfuscated.py
    Verifies if java filenames are obfuscated

--- isObfuscated.py
+++ isObfuscated.py
@@ -1,46 +1,54 @@
 import fnmatch
 import os
-import glob
+#colorama is used for printing a coloured string in command line
 from colorama import init
 import subprocess
 import os.path
 import random
-
+import sys
 init()
-
-rootdir = './Extract/'
-
+# var represents the android .har file we will work with
+var = input("Please enter a jar file: ")
+rootdir = './Extract_'+var+'/'
+#the rootdirectory in which we will work
+print (rootdir)
 
 class isObfuscated:
 	def obf_files(self):
-		count = 0
-		obfuscated = 0
-		not_obfuscated = 0
+		filename  = open("IsObfuscated.txt",'w')
+		sys.stdout = filename
+		count = 0 #total number of java files
+		obfuscated = 0 #total number of obfuscated files
+		not_obfuscated = 0 #total number of non-obfuscated files
+		#searching in all subfolders
 		for subdir, dirs, files in os.walk(rootdir):
 			for file in files:
 				if(fnmatch.fnmatch(file, '*.java')):
+					#counting the number of java files
 					count+=1
-					#print(file)
+					# if the filename is lowercase and has just one letter or a random combination of lowercase letters
 					if (fnmatch.fnmatch(file, '[a-z].java') and file.islower()) or fnmatch.fnmatch(file,'[a-z][a-z]*.java') and file.islower():
-						print('Obfuscated file',file)
+						#number of obfuscated elements increases
 						obfuscated+=1
+					#if the filename contains lowercase and uppercase letters
 					elif file.islower()==False and file.isupper()==False:
-						print ('Not Obfuscated file',file)
+						#number of non obfuscated files increases
 						not_obfuscated+=1
-					else:
-						alone = file
-		print ('Alone',alone)
-		print(count)
+		print('Total number of java files = ',count)
 		print('No. of obfuscated java files = ', obfuscated)
 		print('No. of not obfuscated java files = ', not_obfuscated)
+		#the percentage of obfuscated files => number of obfuscated files divided by the total number of java files
 		obfuscation_percentage = obfuscated/count*100
 		print ('Obfuscation percentage',obfuscation_percentage, '%')
+		#if percentage under 33% => not good
 		if obfuscation_percentage < 33:
 			print('\033[31m' + 'Filenames are not obfuscated')
+		#if percentage over 33% but under 66% => better than nothing
 		elif 33<obfuscation_percentage < 66:
 			print ('Filenames are not obfuscated enough')
 		else:
+		#if percentage above 66% => good enough
 			print('\033[32m' + 'Filenames are obfuscated')
 			
-test1=isObfuscated()
-test1.obf_files()
+test=isObfuscated()
+test.obf_files()
